bc4edad472528c35c7176c09d1922c031d5d6dcd
Q. Price <priceqp@gmail.com>
2019-07-09 13:09:12 -0500

    Add defaultProps to App component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,48 @@
 import React from 'react';  
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>App</h1>
-    </div>
-  );
+class App extends React.Component {
+  static defaultProps = {
+    dogs: [
+      {
+        name: "Whiskey",
+        age: 5,
+        src: 'whiskey',
+        facts: [
+          "Whiskey loves eating popcorn.",
+          "Whiskey is a terrible guard dog.",
+          "Whiskey wants to cuddle with you!"
+        ]
+      },
+      {
+        name: "Hazel",
+        age: 3,
+        src: 'hazel',
+        facts: [
+          "Hazel has soooo much energy!",
+          "Hazel is highly intelligent.",
+          "Hazel loves people more than dogs."
+        ]
+      },
+      {
+        name: "Tubby",
+        age: 4,
+        src: 'tubby',
+        facts: [
+          "Tubby is not the brightest dog",
+          "Tubby does not like walks or exercise.",
+          "Tubby loves eating food."
+        ]
+      }
+    ]
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>App</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
