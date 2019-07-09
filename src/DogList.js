2a6a97ba5a64d9a344ca3115f43fcdc39d705932
Q. Price <priceqp@gmail.com>
2019-07-09 18:34:52 -0500

    Add DogList component, pass dogs array as props in App component render, add images to src folder

---
+++ src/DogList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class DogList extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Dog List!</h1>
+      </div>
+    );
+  }
+}
+
+export default DogList;
