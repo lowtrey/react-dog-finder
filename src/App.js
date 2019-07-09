56d2f40e9b54df6abae96c4324aa396a96a16cc6
Q. Price <priceqp@gmail.com>
2019-07-09 18:14:24 -0500

    Clean slate for complete code refactor using bootstrap

--- src/App.js
+++ src/App.js
@@ -1,8 +1,4 @@
 import React from 'react';
-import Nav from './Nav';
-import Dog from './Dog';
-import Dogs from './Dogs';
-import { Switch, Route } from 'react-router-dom';  
 import './App.css';
 
 class App extends React.Component {
@@ -42,21 +38,45 @@ class App extends React.Component {
   }
 
   render() {
-    let links = this.props.dogs.map(dog => (dog.name));
     return (
-      <div className='App'>
-        <Nav links={links} />
-        <Switch>
-          <Route exact path='/dogs' render={() => <Dogs allDogs={this.props.dogs} />} />
-          <Route 
-            path="/dogs/:dogName"
-            render={(routeProps) => {
-              let dogObj = this.props.dogs.filter(dog => 
-                (dog.name.toLowerCase() === routeProps.match.params.dogName))[0];
-              return <Dog dog={dogObj} />;
-            }} 
-          />
-        </Switch>
+      <div>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+          <a class="navbar-brand" href="#">Navbar</a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+              <li class="nav-item active">
+                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  Dropdown
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <a class="dropdown-item" href="#">Action</a>
+                  <a class="dropdown-item" href="#">Another action</a>
+                  <div class="dropdown-divider"></div>
+                  <a class="dropdown-item" href="#">Something else here</a>
+                </div>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+              </li>
+            </ul>
+            <form class="form-inline my-2 my-lg-0">
+              <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+              <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+            </form>
+          </div>
+
+        </nav>
+        <h1 className='display-1'>Dog App!</h1>
       </div>
     );
   }
