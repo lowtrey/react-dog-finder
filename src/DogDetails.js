3b4c23b218dd6d5ade26c07bbab8eb98edff5ca1
Q. Price <priceqp@gmail.com>
2019-07-09 21:15:47 -0500

    Add and style DogDetails card using bootstrap

--- src/DogDetails.js
+++ src/DogDetails.js
@@ -5,13 +5,20 @@ class DogDetails extends React.Component {
   render() {
     let {dog} = this.props;
     return (
-      <div className='DogDetails row'>
-        <div className='col-11 col-5-lg'>
-          <div className='DogDetails-card card'>
-            <img className='card-img-top' src={dog.src} alt={dog.name} />
+      <div className='container'>
+        <div className='DogDetails row'>
+          <div className='col-11 col-lg-5'>
+            <div className='DogDetails-card card'>
+              <img className='card-img-top' src={dog.src} alt={dog.name} />
+              <div className='card-body'>
+                <h2 className='card-title'>{dog.name}</h2>
+                <h4 className='card-subtitle text-muted'>
+                  {dog.age} years old
+                </h4>
+              </div>
+            </div>
           </div>
         </div>
-        <h1>{this.props.dog.name}</h1>
       </div>
     );
   }
