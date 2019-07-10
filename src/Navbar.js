b244b711ed0a55bd1aba4b1b12dd12c949918186
Q. Price <priceqp@gmail.com>
2019-07-09 22:00:56 -0500

    Add dynamic NavLinks to Navbar component inside render method

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,18 +1,20 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 class Navbar extends React.Component {
   render() {
-    // const dogLinks = this.props.dogs.map(dog => (
-    //   <NavLink to={`/dogs/`} className='nav-link'>
-    //     {dog.name}
-    //   </NavLink>
-    // ))
+    const dogLinks = this.props.dogs.map(dog => (
+      <li className='nav-item' key={dog.name}>
+        <NavLink exact to={`/dogs/${dog.name}`} className='nav-link' >
+          {dog.name}
+        </NavLink>
+      </li>
+    ));
     return (
       <nav className='navbar navbar-expand-lg navbar-dark bg-dark'>
-        <a className='navbar-brand' href='#'>
+        <Link className='navbar-brand' to='/dogs'>
           Dog App
-        </a>
+        </Link>
         <button 
           className='navbar-toggler'
           type='button'
@@ -27,8 +29,11 @@ class Navbar extends React.Component {
         <div className='collapse navbar-collapse' id='navbarNav'>
           <ul className='navbar-nav'>
             <li className='nav-item'>
-              <NavLink to='/dogs' className='nav-link'>Home</NavLink>
+              <NavLink exact to='/dogs' className='nav-link'>
+                Home
+              </NavLink>
             </li>
+            {dogLinks}
           </ul>
         </div>
       </nav>
