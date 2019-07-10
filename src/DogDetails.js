e6a094cd86362cb3b9e4155f7895037c664474ac
Q. Price <priceqp@gmail.com>
2019-07-09 19:41:59 -0500

    Refactor DogDetails component, add DogDetails css stylesheet

--- src/DogDetails.js
+++ src/DogDetails.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import './DogDetails.css';
 
 class DogDetails extends React.Component {
   render() {
+    let {dog} = this.props;
     return (
-      <div>
+      <div className='DogDetails row'>
+        <div className='col-11 col-5-lg'>
+          <div className='DogDetails-card card'>
+            <img className='card-img-top' src={dog.src} alt={dog.name} />
+          </div>
+        </div>
         <h1>{this.props.dog.name}</h1>
       </div>
     );
