a22d7ccdac37875fbe469343c1637bbb41214bbf
Q. Price <priceqp@gmail.com>
2019-07-09 19:01:26 -0500

    Add getDog function to App component render which adds dynamic routing capability

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import DogList from './DogList';
+import DogDetails from './DogDetails';
 import { Switch, Route } from 'react-router-dom';
 import whiskey from './images/whiskey.jpg';
 import tubby from './images/tubby.jpg';
@@ -42,7 +43,19 @@ class App extends React.Component {
     ]
   };
   render() {
-    return <Route path='/dogs' render={() => <DogList dogs={this.props.dogs} />} />;
+    const getDog = props => {
+      let name = props.match.params.name;
+      let currentDog = this.props.dogs.find(
+        dog => dog.name.toLowerCase() === name.toLowerCase()
+      );
+      return <DogDetails {...props} dog={currentDog} />
+    };
+    return (
+      <Switch>
+        <Route exact path='/dogs' render={() => <DogList dogs={this.props.dogs} />} />
+        <Route exact path='/dogs/:name' render={getDog} />
+      </Switch>
+    );
   }
 }
 
